31e0bfbbc2540bdb7fc06cb80cb389930da8b948
Hans Fredrik Furholt <hansff@gmail.com>
2020-01-21 22:59:00 +0100

    optimize emacs config

--- emacs-config/init.el
+++ emacs-config/init.el
@@ -1,2 +1,7 @@
-(package-initialize)
-(org-babel-load-file "~/.emacs.d/config.org")
+;; (package-initialize)
+;; (org-babel-load-file "~/.emacs.d/config.org")
+
+(if (file-exists-p (concat user-emacs-directory "config.el"))
+    (load-file (concat user-emacs-directory "config.el"))
+  (org-babel-load-file "~/.emacs.d/config.org"))
+
