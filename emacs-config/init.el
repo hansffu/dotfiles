4e2216a8fb9a104b2129be4b3fc3e206e7ffde38
Hans Fredrik Furholt <hansff@gmail.com>
2020-08-13 20:14:46 +0200

    delete old emacs config

--- emacs-config/init.el
+++
@@ -1,7 +0,0 @@
-;; (package-initialize)
-;; (org-babel-load-file "~/.emacs.d/config.org")
-
-(if (file-exists-p (concat user-emacs-directory "config.el"))
-    (load-file (concat user-emacs-directory "config.el"))
-  (org-babel-load-file "~/.emacs.d/config.org"))
-
