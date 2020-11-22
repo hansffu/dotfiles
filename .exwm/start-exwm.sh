8f7aafc06d2d63bfa8cd0cbb00fcedf099131e77
Hans Fredrik Furholt <hansff@gmail.com>
2020-11-22 23:12:06 +0100

    emacs - exwm startup fixes

--- .exwm/start-exwm.sh
+++ .exwm/start-exwm.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
-export _JAVA_AWT_WM_NONREPARENTING=1
+# this makes it work in Ubuntu
+xhost +
+## you might need to append the TTY you are working on
+xinit
 
-exec dbus-launch --sh-syntax --exit-with-session emacs -mm --debug-init
+wmname LG3D
+export _JAVA_AWT_WM_NONREPARENTING=1
+# export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus"
+
+export VISUAL=emacsclient
+export EDITOR="$VISUAL"
+
+# exec dbus-launch --sh-syntax --exit-with-session emacs --eval "(exwm-enable)"
+exec emacs --eval "(exwm-enable)"
